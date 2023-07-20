0a6d358244376d00c43c3c3aeb3ffe8d9e6f30bc
gabrielemafrica <gabriele.ga@live.it>
2023-07-20 18:22:16 +0300

    home

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Project;
+
+class MainController extends Controller
+{
+    public function index() {
+
+        $projects = Project :: all();
+
+        return view("index", compact('projects'));
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="container text-center py-5">
+        <h1>I MIEI PROGETTI</h1>
+        <ul class="list-unstyled d-flex flex-wrap gap-3 justify-content-center">
+            @foreach ($projects as $project)
+                <li class="mt-3 w-25">
+                    <div class="border border-primary py-4">
+                        {{ $project->nome }}
+                        <br>
+                        <a href="{{ route('show', $project->id) }}" class="btn btn-info">SHOW</a>
+
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </main>
+@endsection
